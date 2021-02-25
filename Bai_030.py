2ba26ed9424c98092b34f7dbd6674903a460c14f
HuynhLoc19 <79509556+HuynhLoc19@users.noreply.github.com>
2021-02-25 09:24:59 +0700

    Update Bai_030.py

--- Bai_030.py
+++ Bai_030.py
@@ -12,3 +12,8 @@ if Toan_Le(n)==0:
     print(n,'khong toan le.')
 else:
     print(n, 'toan le.')
+
+print('Loc dep trai vai dai')
+print('Cuoc song nay chi co lam thi moi co an')
+print('Khong lam ma doi co an thi chi an dau bua, an cam.')
+print('You have my word...')
