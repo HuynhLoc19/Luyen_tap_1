23d5d4f36f345f556b7607ffcc137ef4ddf247a5
HuynhLoc <loc.huynh19@hcmut.edu.vn>
2021-02-25 09:23:25 +0700

    Bai_030

---
+++ Bai_030.py
@@ -0,0 +1,14 @@
+
+# Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không
+
+def Toan_Le(n):
+    if n==0:
+        return 1
+    if (n%10)%2==0:
+        return 0
+    return Toan_Le(n//10)
+n = int(input('Nhập n: '))
+if Toan_Le(n)==0:
+    print(n,'khong toan le.')
+else:
+    print(n, 'toan le.')
